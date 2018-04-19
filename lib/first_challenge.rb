fdf950413ee024823fbbb3af27bb71df189c242d
Marie Sonko <mariesonko62@gmail.com>
2018-04-19 22:00:05 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -13,7 +13,7 @@ def first_challenge
     }
   }
 
-freddy = contacts["Freddy Mercury"]
+  freddy = contacts["Freddy Mercury"]
   freddy.each do |attribute, value|
         if attribute == :favorite_icecream_flavors
         value.delete_if{ |ice_cream| ice_cream == "strawberry" }
